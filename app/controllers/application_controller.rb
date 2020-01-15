b938960ed8c561c3b591842d11609a625fd16267
Eric <spb828@naver.com>
2020-01-15 22:54:56 +0900

    모델 업데이트 및 locale 에러 수정

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::MimeResponds
   respond_to :json
+  around_action :set_current_user
+
+  # https://stackoverflow.com/a/2513456
+  def set_current_user
+    Model::Current.user = current_user
+    yield
+  ensure
+    # to address the thread variable leak issues in Puma/Thin webserver
+    Model::Current.user = nil
+  end    
 end
