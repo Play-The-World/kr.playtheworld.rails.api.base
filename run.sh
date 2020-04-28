919d02227f5241c9ebfe699b5ed9a2cf2b194b41
Eric <spb828@naver.com>
2020-04-28 12:13:20 +0900

    fix indexing, globalize

--- run.sh
+++ run.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-export FONT_BOLD=$(tput bold)
-export FONT_NORMAL=$(tput sgr0)
-
-# GET BUILD ENVIRONMENT
-echo "Select an environment (${FONT_BOLD}dev${FONT_NORMAL} | test | prod):"
-read BUILD_ENV
-
-export BUILD_ENV="$(echo "$BUILD_ENV" | tr -d '[:space:]')"
-
-# SET DEFAULT VALUES
-if [ "$BUILD_ENV" = "" ]; then
-  export BUILD_ENV="dev"
-fi
-
-if [ "$BUILD_ENV" = "dev" ]; then
-  docker-compose -f .configs/dev/docker-compose.yml up
-elif [ "$BUILD_ENV" = "test" ]; then
-  echo "test"
-elif [ "$BUILD_ENV" = "prod" ]; then
-  echo "prod"
-else
-  echo "WRONG ENV '$BUILD_ENV'"
-fi
-
-# UNSET ENVIRONMENT VALUES
-unset FONT_BOLD
-unset FONT_NORMAL
-unset BUILD_ENV
+PS3='Select an environment:'
+build_envs=("dev" "test" "prod")
+select env in "${build_envs[@]}"
+do
+  case $env in
+    "dev")
+      docker-compose -f .configs/dev/docker-compose.yml up
+      break
+      ;;
+    "test")
+      echo "test"
+      break
+      ;;
+    "prod")
+      echo "prod"
+      break
+      ;;
+    *) echo "invalid environment. $REPLY";;
+  esac
+done
