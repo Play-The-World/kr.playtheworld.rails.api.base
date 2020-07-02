16ea565badcb80399cfde3271f9f835a3a4c5dfd
Eric <spb828@naver.com>
2020-07-02 17:07:10 +0900

    왜 안될까..?

--- .configs/production/start-up.sh
+++ .configs/production/start-up.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Wait for DB services
-sh ./.configs/production/wait-for-services.sh
+# sh ./.configs/production/wait-for-services.sh
 
 # Prepare DB (Migrate - If not? Create db & Migrate)
 # sh ./.configs/production/prepare-db.sh
@@ -10,5 +10,5 @@ sh ./.configs/production/wait-for-services.sh
 # sh ./asset-pre-compile.sh
 
 # Start Application
-# bundle exec puma -C ../../../config/puma.rb
-bundle exec rails server -b 0.0.0.0 -p 3000
+bundle exec puma -C config/puma.rb
+# bundle exec rails server -b 0.0.0.0 -p 3000
