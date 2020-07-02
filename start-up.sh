974729e4dc4161020c695f0c2bb98fa57b68eefe
Eric <spb828@naver.com>
2020-07-02 18:12:10 +0900

    fix nginx

---
+++ start-up.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Wait for DB services
+sh ./.configs/production/wait-for-services.sh
+
+# Prepare DB (Migrate - If not? Create db & Migrate)
+# sh ./.configs/production/prepare-db.sh
+
+# Pre-comple app assets
+# sh ./asset-pre-compile.sh
+
+# Start Application
+bundle exec puma -C config/puma.rb
+# bundle exec rails server -b 0.0.0.0 -p 3000
