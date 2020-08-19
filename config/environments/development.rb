a13405dae04e38cc018d0989584f0bb426d2e26d
Eric <spb828@naver.com>
2020-08-19 14:28:34 +0900

    mailer: gmail 연동

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,20 @@ Rails.application.configure do
 
   # config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name:            Rails.application.credentials.gmail[:user_name],
+      password:             Rails.application.credentials.gmail[:password],
+      authentication:       'plain',
+      enable_starttls_auto: true 
+    }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_options = {
+    from: 'no-reply@playthe.world'
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
